b70651be352198bfd5694addb3e5e051a8a146bc
Vitor Oliveira <vitorroliveiraa@outlook.com.br>
2024-10-03 08:53:39 -0300

    feat: dotenv lib add

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import path from "path";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import "dotenv/config";
 
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
