a04e6bace280172a140779639f53739cd863180b
Vitor Oliveira <vitorroliveiraa@outlook.com.br>
2025-06-27 15:29:23 -0300

    style: update button color in register page for improved visibility

--- src/modules/auth/register/pages/register-page.tsx
+++ src/modules/auth/register/pages/register-page.tsx
@@ -316,7 +316,7 @@ export default function RegisterPage() {
               </Button>
               <Button
                 type="submit"
-                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                className="bg-slate-800 hover:bg-slate-700 text-white font-bold py-2 px-4 rounded"
                 disabled={mutation.isPending}
               >
                 {mutation.isPending ? (
